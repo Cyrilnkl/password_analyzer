de68e209802705e5af2a4ff715de966973bf230a
Cyrilnkl <cyrilnkl5@gmail.com>
2024-03-26 16:36:40 +0100

    check special characters

--- analyzer.py
+++ analyzer.py
@@ -1,2 +1,8 @@
-def checkSpecialCharacters(password):
-    specialChar = ""
+def checkSpecialCharacters(password, score):
+    specialChar = "!@#$%^&*()-_=+{}[]|:;'\"";
+
+    for i in range (0, len(password)):
+        if (password[i] in specialChar) : 
+            print("oui : ", password[i])
+    return 0
+
---
+++ main.py
@@ -0,0 +1,7 @@
+from analyzer import *
+
+if __name__ == "__main__":
+    score = 0;
+    password = "he&&y@"
+    print("go")
+    score = checkSpecialCharacters(password, score)
